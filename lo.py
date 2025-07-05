f91a9e777f9e51b5aedf6dd280a31a6dcd709f5c
ᛕꪖꪶᛕﺃ ༗ <surajkambozdhot@gmail.com>
2025-07-05 14:00:19 +0530

    Update lo.py

--- lo.py
+++ lo.py
@@ -70,9 +70,9 @@ def pull_run(work, cmds):
 	        print(result) 
 bot = Client(
   "CW",
-  bot_token= "6917084113:AAFOK37UIoEQLtY2HYu5UeF77cIF4BfGnwk",
-  api_id= 27097807 ,
-  api_hash= "9fd790a9cb1f639c921d941621d2959d" 
+  bot_token= "",
+  api_id= 29378064 ,
+  api_hash= "44cf2bea8cbe0ec4e383427aab3fa934" 
 )
 
 @bot.on_message(filters.command(["down"]) & ~filters.edited)
